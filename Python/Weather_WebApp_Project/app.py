deab6a848445ca939b7d69af3ebf515a3c171dbf
or.toledano <toldor811@gmail.com>
2024-10-29 07:45:33 +0200

    Logger Project Updated

--- Python/Weather_WebApp_Project/app.py
+++ Python/Weather_WebApp_Project/app.py
@@ -34,9 +34,7 @@ limiter = Limiter(
 logging.basicConfig(
     filename='weather_app.log',
     level=logging.INFO,
-    format='%(asctime)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
-    encoding='utf-8'
 )
 
 # Create logger
