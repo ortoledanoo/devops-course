7459848e9cbff2a5fd6dcd825371cc0080e13cd7
or.toledano <toldor811@gmail.com>
2024-12-15 09:37:58 +0200

    Linux_3

--- Quiz/Linux_3/soultion.sh
+++ Quiz/Linux_3/soultion.sh
@@ -3,16 +3,12 @@ tail -n +99 file.txt | head -n 1
 # head -n 1 ||  Extracts only the first line of the output
 
 chmod 1770 shared_directory
-
 # Create Shared Directory
 # 1 (Sticky Bit) Prevents users from deleting files they not owners
 
-
 scp local_file dest_user@dest_ip:/path/on/dest
 
-
 command < file1 > file2 2> file3
-
 # < file1 || Redirects input to the command from file1
 # > file2: Redirects stdout to file2
 # 2> file3: Redirects stderr to file3
